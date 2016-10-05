adf78030f9be07eea2f156664a72d73babcb5bec
peggymercyprescott <peggymercyprescott@hibecki.pe.hu>
2016-10-05 04:22:01 -0400

    test06-tcp-echo-server.c added

--- test06-tcp-echo-client.c
+++ test06-tcp-echo-client.c
@@ -10,21 +10,21 @@
 #define DEFAULT_BACKLOG 1024
 
 uv_loop_t *loop;
+uv_tcp_t* uvsocket;
 
-void on_close(uv_handle_t* handle) {
-  
-}
-
-void cb(uv_write_t* req, int status) {
-  uv_close(req->handle, on_close);
+void on_write(uv_write_t* req, int status) {
+  if (status < 0) {
+    fprintf(stderr, "on write error %s\n", uv_strerror(status));
+    return;
+  }
+  uv_close((uv_handle_t*)uvsocket, NULL);
 }
 
 void on_connect(uv_connect_t* req, int status) {
-    if (status < 0) {
-        fprintf(stderr, "New connection error %s\n", uv_strerror(status));
-        // error!
-        return;
-    }
+  if (status < 0) {
+    fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+    return;
+  }
   
 uv_buf_t a[] = {
   { .base = "O", .len = 1 },
@@ -32,22 +32,22 @@ uv_buf_t a[] = {
 };
 uv_write_t req1;
   
-uv_write(&req1, req.handle, a, 2, cb);
+uv_write(&req1, req->handle, a, 2, on_write);
 
 }
 
 int main() {
     loop = uv_default_loop();
   
-uv_tcp_t* socket = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-uv_tcp_init(loop, socket);
+uvsocket = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+uv_tcp_init(loop, uvsocket);
 
 uv_connect_t* connect = (uv_connect_t*)malloc(sizeof(uv_connect_t));
 
 struct sockaddr_in dest;
 uv_ip4_addr("127.0.0.1", 13370, &dest);
 
-uv_tcp_connect(connect, socket, (const struct sockaddr*)&dest, on_connect);
+uv_tcp_connect(connect, uvsocket, (const struct sockaddr*)&dest, on_connect);
   
   
     return uv_run(loop, UV_RUN_DEFAULT);
